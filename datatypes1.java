1297d52301e1e47c809d6048b6929527e85cd6db
fatsfa <fathifathima808@gmail.com>
2024-05-14 19:03:24 +0530

    tst

--- datatypes1.java
+++ datatypes1.java
@@ -4,7 +4,7 @@ class test{
     public static void main(String[] args){
         int loc = 10000;
         test e1 = new test();
-        System.out.println("hi"+a);
+        System.out.println("STATIC"+a);
         System.out.println(loc);
         System.out.println(e1.inst);
     }
